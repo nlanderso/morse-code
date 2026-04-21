41dbf1f4357edf04cf7f8b0523eb53f754d13e64
Nancy Anderson <nlanders@andrew.cmu.edu>
2026-04-21 15:13:51 -0400

    Updated project.v and related files

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,22 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+Morse code is input as a button press. A hold for one second is a "dit" and a 
+hold for three seconds is a "dah". A release for one second distinguishes the 
+end of a dit or dah, a release for three seconds is the end of a letter, and a
+release for seven seconds is the end of a word. The button press is translated
+into A-Z and output as seven-segment encodings to be displayed on two seven
+segment displays. An external LED blinks at a 1 second rate to help users 
+keep time.
 
 ## How to test
 
-Explain how to use your project
+Use an external button to input the Morse code. Use an external LED to help
+keep time as you input. The decoded letters should appear on the external 
+seven segment displays
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+Button
+LED
+Digilent Pmod SSD
--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Nancy Anderson
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_nlanderso_morse_code (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,24 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0;
+  // assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in[7:0], ui_in[7:1], 1'b0};
+
+  wire [7:0] led_out;
+
+  // Button press is the input
+  // Output 8 bits to control the seven segments
+  // Output 8 LED bits but only care about the LSB
+  ChipInterface morse_code(.AA(uo_out[0]), .AB(uo_out[1]), .AC(uo_out[2]), 
+                           .AD(uo_out[3]), .AE(uo_out[4]), .AF(uo_out[5]), 
+                           .AG(uo_out[6]), .CAT(uo_out[7]), .led(led_out),
+                           .clock(clk), .reset_n(rst_n), .btn_up(ui_in[0]));
+  assign uio_oe = 8'h01;
+  assign uio_out[7:1] = 7'b0;
+  assign uio_out[0] = led_out[0];
 
 endmodule
